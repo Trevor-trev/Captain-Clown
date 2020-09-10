5590f95f2af623c8f0015a38a14b26d4067812c5
Trevor-trev <64917730+Trevor-trev@users.noreply.github.com>
2020-09-10 14:28:26 -0500

    Added a Timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public float secondsCount;
+    public int minuteCount;
+    public int hourCount;
+    void FixedUpdate()
+    {
+        UpdateTimerUI();
+    }
+    public void UpdateTimerUI()
+    {
+        secondsCount += Time.fixedDeltaTime;
+        timerText.text = hourCount + "h:" + minuteCount + "m:" + (int)secondsCount + "s";
+        if (secondsCount >= 60)
+        {
+            minuteCount++;
+            secondsCount %= 60;
+            if (minuteCount >= 60)
+            {
+                hourCount++;
+                minuteCount %= 60;
+            }
+        }
+    }
+    }
