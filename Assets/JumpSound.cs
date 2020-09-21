c5939187049f85822568efdc4ae05cc78b826d28
Trevor-trev <64917730+Trevor-trev@users.noreply.github.com>
2020-09-21 16:31:59 -0500

    added more sounds!

---
+++ Assets/JumpSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpSound : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string jumpSound;
+
+    public Playermovement pmov;
+    public GroundCheck groundCheck;
+
+    private void Update()
+    {
+        if ((pmov.onPole || groundCheck.grounded) && Input.GetButtonDown("Jump"))
+            FMODUnity.RuntimeManager.PlayOneShot(jumpSound);
+    }
+}
---
+++ Assets/PogoSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PogoSound : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string pogoSound;
+
+    public Playermovement pmov;
+    public GroundCheck groundCheck;
+
+    private void FixedUpdate()
+    {
+        if ((pmov.pogo && groundCheck.grounded))
+            FMODUnity.RuntimeManager.PlayOneShot(pogoSound);
+    }
+}
