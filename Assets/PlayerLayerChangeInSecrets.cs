9cdc9ad918ca0557967abb711d6373d974bf146c
Trevor-trev <64917730+Trevor-trev@users.noreply.github.com>
2020-09-22 10:36:38 -0500

    layer issues in secrets fixed

---
+++ Assets/PlayerLayerChangeInSecrets.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerLayerChangeInSecrets : MonoBehaviour
+{
+
+    public bool insideSecret;
+    public void OnTriggerStay2D(UnityEngine.Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            insideSecret = true;           
+    }
+
+    public void OnTriggerExit2D(UnityEngine.Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            insideSecret = false;
+    }
+}
--- Assets/Scripts/Characters/Player/PlayerLayerChange.cs
+++ Assets/Scripts/Characters/Player/PlayerLayerChange.cs
@@ -13,6 +13,7 @@ public class PlayerLayerChange : MonoBehaviour
       This is only relavent if the game is in a tilted, "2.5D" perspective
       much like Commander Keen, in which these scripts were originally intended to mimic.*/
 
+    public PlayerLayerChangeInSecrets inSecret;
     public Playermovement pmov;
     private SpriteRenderer sprite;//--------------------------A reference to the sprite renderer on the same object that this script is attached to
     private bool dontChangeLayer;//---------------------------A bool that determines when to change the player's sorting layer
@@ -38,7 +39,7 @@ public class PlayerLayerChange : MonoBehaviour
         if (pmov.facingRight && !pmov.onPole)//---If the character is facing right while not on a pole
             sprite.sortingOrder = 1;//------------Set the character's order within it's current layer to 1
 
-        if (pmov.facingLeft && !pmov.onPole)//----If the character is facing left while not on a pole
+        if (pmov.facingLeft && !pmov.onPole && !inSecret.insideSecret)//----If the character is facing left while not on a pole
             sprite.sortingOrder = 3;//------------Set the character's order within it's current layer to 3
 
         if (pmov.onPole || dontChangeLayer)//------------------------If the character is on a pole
@@ -46,5 +47,8 @@ public class PlayerLayerChange : MonoBehaviour
 
         if (!pmov.onPole && !dontChangeLayer)//-----------------------If the character is not on a pole
             sprite.sortingLayerName = "Default";//Set the character's sorting layer to "Default"
+
+        if (inSecret.insideSecret)
+            sprite.sortingOrder = 1;
     }
 }
