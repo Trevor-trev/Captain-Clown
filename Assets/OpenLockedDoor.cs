dccaba3cf2d9216dec0af6e1b13df38e694a46cc
Trevor-trev <64917730+Trevor-trev@users.noreply.github.com>
2020-12-23 08:17:02 -0600

    working on getting keys and doors to work.
    Need to work on acquiring items first

---
+++ Assets/OpenLockedDoor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLockedDoor : MonoBehaviour
+{
+    public BoxCollider2D doorCollider;
+    private SpriteRenderer sprite;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    void DisableCollider()
+    {
+        doorCollider.enabled = false;
+        sprite.sortingLayerName = "Pole";
+    }
+}
+
