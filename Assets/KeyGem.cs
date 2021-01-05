6cabc800fdd40eb09a027812399fdc75ccfee470
Trevor-trev <64917730+Trevor-trev@users.noreply.github.com>
2021-01-05 16:30:03 -0600

    Gem Collected code is done! just need to add the proper sprite

--- Assets/KeyGem.cs
+++ Assets/KeyGem.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class KeyGem : MonoBehaviour
 {
+    public SpriteRenderer spriteRenderer;
+    public Sprite collectedSprite;
+    public Rigidbody2D rb;
+    public float speed;
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Inventory"))
-            Destroy(gameObject);
+            StartCoroutine(Collected());
+    }
+
+    void ChangeSprite()
+    {
+        spriteRenderer.sprite = collectedSprite;
+        rb.velocity = new Vector2(0, speed * Time.deltaTime);
+    }
+    IEnumerator Collected()
+    {
+        ChangeSprite();
+        yield return new WaitForSeconds(.5f);
+        Destroy(gameObject);
     }
 }
